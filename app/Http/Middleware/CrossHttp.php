99a3e9e4ce8c11cbfb7c8555604c09e22901a6c8
lujiang <jiang.lu@uuabc.com>
2018-10-09 15:26:21 +0800

    增加跨域中间件

---
+++ app/Http/Middleware/CrossHttp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CrossHttp
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $wxUrl    = getRealEnv('wechat.JD_PROJECT_URL');
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', $wxUrl);
+        $response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Access-Control-Request-Headers, SERVER_NAME, Access-Control-Allow-Headers, cache-control, token, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie');
+
+        return $response;
+    }
+}
