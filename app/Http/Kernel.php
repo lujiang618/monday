92dcb53d935b65624482a4c6d1085c7dcb019151
lujiang <531432594@qq.com>
2018-10-11 16:33:43 +0800

    跨域中间件要写成全局中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\CrossHttp::class,
     ];
 
     /**
@@ -57,7 +58,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-
-        'cross' => \App\Http\Middleware\CrossHttp::class,
+        
     ];
 }
