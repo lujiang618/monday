42d8a5d01829c8c46a4214fd87c58e79f4801829
lujiang <531432594@qq.com>
2018-12-05 11:44:44 +0800

    router

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
+use Illuminate\Routing\Router;
+use \App\Supports\StringHelper;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/**
+Route::group([
+    'namespace'=> 'External',
+    'prefix'=>'',
+],function (Router $router){
+    $router->post("/captcha", "CaptchaController@getCaptcha")->name("captcha");
+});
+ */
+
+
+if (env('APP_ENV') === 'local') {
+    // for testing, not in use
+    Route::get('/i', function () {
+        phpinfo();
+    });
+
+
+    Route::get('/test', function () {
+        echo StringHelper::uuid();
+    });
+}
